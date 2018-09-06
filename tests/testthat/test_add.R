3ce14d03c13e60e14bb73706a1244338b8aaf189
Brian Masinde <masindeb@live.com>
2018-09-06 16:42:09 +0200

    modified test_add

--- tests/testthat/test_add.R
+++ tests/testthat/test_add.R
@@ -2,12 +2,12 @@
 context("add")
 
 test_that("User Input Validation", {
-  expect_error(add("7a", 5),12)
+  expect_error(add("7a", 5))
 }
 )
 
 test_that("Function Validation",{
   expect_equal(add(7, 5),12)
-  expect_unequal(add(2,3),4)
+  expect_false(isTRUE(all.equal(add(2,9),12)))
 }
 )
