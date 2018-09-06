9755963a40e1a23d2b937fd30fa81862d73c8302
Brian Masinde <masindeb@live.com>
2018-09-06 17:03:44 +0200

    Solved documentation

--- R/add.R
+++ R/add.R
@@ -1,3 +1,12 @@
+#' Function to sum
+#'
+#' @param a This is the first parameter as numeric
+#' @param b this is the second parameter as numeric
+#'
+#' @return sum of a and b
+#' @export
+#'
+#' @examples add(2,5)
 add <- function(a,b){
   if(is.numeric(a) == FALSE || is.numeric(b) == FALSE){
     stop(" a or b is NOT numeric")
